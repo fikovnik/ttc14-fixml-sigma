75c3eb5249a8be01827d339551e3fc0ca5ab6842
Filip Krikava <krikava@gmail.com>
2014-04-25 15:29:53 +0200

    Added base solution without data type guessing

---
+++ ttc14-fixml/ttc14-fixml-base/src/fr/inria/spirals/sigma/ttc14/fixml/ObjLang2CPPBase.scala
@@ -0,0 +1,27 @@
+package fr.inria.spirals.sigma.ttc14.fixml
+
+import fr.inria.spirals.sigma.ttc14.fixml.objlang.support.ObjLang
+import fr.inria.spirals.sigma.ttc14.fixml.objlang.support.ObjLang._objlang._
+
+trait ObjLang2CPPBase extends ObjLang2Code {
+
+  var cppHeaderSuffix = "h"
+
+  implicit class ClassCPPUtil(that: Class) {
+    def cppHeaderFile = s"${that.name}.${cppHeaderSuffix}"
+  }
+
+  override def toCode(e: NullLiteral) = "NULL"
+
+  override def type2Code(e: TypedElement): String = (e.type_.isInstanceOf[Class], e.many) match {
+    case (true, false) => class2Code(e.type_) + "*"
+    case (true, true) => class2Code(e.type_) + "**"
+    case (false, true) => class2Code(e.type_)+ "[]"
+    case (false, false) => class2Code(e.type_)
+  }
+
+  override def toCode(a: ArrayLiteral): String =
+    s"new ${class2Code(a.type_)}*[${a.elements.size}] { ${a.elements map (toCode) mkString (", ")} }"
+
+  override def class2Code(p: DataType) = "std::string"
+}
